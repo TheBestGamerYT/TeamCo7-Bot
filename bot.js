20cd652f54f65c52b7bcdc92eeb7c70832280e9c
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2018-12-10 14:19:58 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,13 +7,8 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('pong! I am still under Development! So please wait for further progress of my Development Organization. Join Gaming Discord: https://discord.gg/67gbBKH');
+    	message.reply('pong! This bot still under development by TeamCo7 Developments (restricted server)! Join public gaming Discord server: https://discord.gg/67gbBKH');
   	}
-client.on('message', message => {
-    if (message.content === 'botinfo') {
-        message.reply('My bot ID: 481540507479048203 | I am still under Development by TeamCo7 Developments (restricted server)') ;
-    }
-                  
 });
 
 //
