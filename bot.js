4bd322d3daa7f8abdb1d9c4ac00c8587f5da83cd
TheBestGamerYT <franci641@gmail.com>
2018-12-10 20:42:22 +0100

    Update of bot.js
    I've updated it so playing thing + help menu is there plus prefix works

--- bot.js
+++ bot.js
@@ -1,17 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+client.on('ready',() => {
+  console.log("I\'m Online !");
+  client.user.setActivity('b! is prefix', { type: 'PLAYING' })
+    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'b! is prefix'}`))
+    .catch(console.error);
+})
 
 var prefix = "!"
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong! This bot still under development by TeamCo7 Developments (restricted server)! Join public gaming Discord server: https://discord.gg/67gbBKH');
+  if (message.author === client.user) return;
+  	if (message.content.startsWith(prefix + 'ping')) {
+  		message.channel.send('pong');
+  	}
+    if (message.content.startsWith(prefix + 'help')) {
+  		message.channel.send('Welcome to a help page ! \n b! ping - pong \n b! help - help page \n This bot still under development by TeamCo7 Developments (restricted server)!  \n Join public gaming Discord server: https://discord.gg/67gbBKH');
   	}
 });
-
-//
+// My version changed and hopefully now works delete this comment when you changed the code and your are not TheBestGamerYT
 client.login(process.env.BOT_TOKEN);
