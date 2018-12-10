0e58214402f7dd31054fbcce8170b6bab7cd487b
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2018-12-10 17:00:17 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
 
-var prefix = "b!"
-
-client.on('ready',() => {
-  console.log("I\'m Online !");
-  client.user.setActivity('b! is prefix', { type: 'PLAYING' })
-    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'b! is prefix'}`))
-    .catch(console.error);
-})
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
 client.on('message', message => {
-  if (message.author === client.user) return;
-  	if (message.content.startsWith(prefix + 'ping')) {
-  		message.channel.send('pong');
+    if (message.content === 'ping') {
+    	message.reply('pong');
   	}
-    if (message.content.startsWith(prefix + 'help')) {
-  		message.channel.send('Welcome to a help page ! \n b! ping - pong \n b! help - help page');
-  	}
-  });
-client.login(config.token);
+});
+
+// 
+client.login(process.env.BOT_TOKEN);
