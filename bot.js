76584956abeda5360da740f2e7fe45f106de5cb9
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2018-12-14 20:28:46 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,17 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "!c7 "
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log('I am ready!');
-    client.user.setActivity('! is prefix', { type: 'PLAYING' })
- .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : '! is prefix'}`))
- .catch(console.error);
- });
 
-client.on('message', msg => {
-    if (msg.content.startsWith(prefix + 'ping')) {
-        msg.channel.send(`Pong! \`${Date.now() - msg.createdTimestamp} ms\``);
-  }
+client.on('ready', () => {
+    console.log('I am ready!');
+client.user.setActivity('! is prefix', { type: 'PLAYING' }) .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : '! is prefix'}`)) .catch(console.error);
 });
 
-//
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// 
 client.login(process.env.BOT_TOKEN);
