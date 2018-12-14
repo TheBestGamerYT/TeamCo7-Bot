dd74f64d25079b9e53810c1386b3f00d97d5e60a
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2018-12-14 16:24:25 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "! "
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content(prefix + 'ping')) {
-    	message.reply('pong! Bot is globally under maintenance and is being updated and recoded. Under the authority of FinalSquad Company, Dept. of TeamCo7 Organization & Developments. We advise bot users to please be patiënt');
+    if (message.content === 'ping') {
+    	message.reply('pong');
   	}
 });
 
 // 
 client.login(process.env.BOT_TOKEN);
-
-
