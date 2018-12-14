f0115494123d19384a73cf55c9daf64fac885e41
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2018-12-14 16:26:25 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('pong');
+    	message.reply('ping pong bitch!');
   	}
 });
 
