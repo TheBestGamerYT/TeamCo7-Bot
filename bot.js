7ad942fa09884fda2bce070c558361cdcffe9101
TheBestGamerYT <franci641@gmail.com>
2018-12-14 21:46:04 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,16 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var prefix = "!c7 "
+
 client.on('ready', () => {
-    console.log('I am ready!');
-});
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log('I am ready!');
+    client.user.setActivity('! is prefix', { type: 'PLAYING' })
+ .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : '! is prefix'}`))
+ .catch(console.error);
+ });
 
-var prefix = "! "
-
-client.on('message', message => {
-    if (message.content.startsWith(prefix + 'ping')) {
-    	message.channel.send('pong! This bot still under development by TeamCo7 Developments (restricted server)! Join public gaming Discord server: https://discord.gg/67gbBKH');
-  	}
+client.on('message', msg => {
+    if (msg.content.startsWith(prefix + 'ping')) {
+        msg.channel.send(`Pong! \`${Date.now() - msg.createdTimestamp} ms\``);
+  }
 });
 
 //
