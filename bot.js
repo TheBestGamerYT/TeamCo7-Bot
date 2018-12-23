d82ff74c07ccd23291c22d07e4643d1fee3519bc
TheBestGamerYT <franci641@gmail.com>
2018-12-23 18:18:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,32 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = "!c7 "
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+client.on('ready',() => {
+  console.log("I\'m Online !");
+  client.user.setActivity('!c7 is prefix', { type: 'PLAYING' })
+    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : '!c7 is prefix'}`))
+    .catch(console.error);
+})
 
 client.on('message', message => {
-    if (message.content === '!help') {
-    	message.reply('Sorry, this unindependent bot is currently still under development by FinalSquad Company, under the authority of Parent Organization: TeamCo7 Gaming Clan! Contact FinalSquad: Staff, Admins, Developers, CEO or Board of Directors for more information regarding our developments! You can also apply! https://discord.gg/5wwCBEA');
- 	}
+
+  if (message.author === client.user) return;
+  	if (message.content.startsWith(prefix + 'ping')) {
+  		message.channel.send('pong');
+  	}
+    if (message.content.startsWith(prefix + 'help')) {
+  		message.channel.send('Welcome to a help page ! \n !c7 ping - pong \n !c7 help - help page \n This bot still under development by TeamCo7 Developments (restricted server)!  \n Join public gaming Discord server: https://discord.gg/67gbBKH');
+  	}
+});
+// My version changed and hopefully now works delete this comment when you changed the code and your are not TheBestGamerYT
+
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
 // 
+
 client.login(process.env.BOT_TOKEN);
