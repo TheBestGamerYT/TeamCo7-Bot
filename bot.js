9277abf94548da70fc7f52c7398d7cea9f64c47e
TheBestGamerYT <franci641@gmail.com>
2019-01-02 17:55:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,9 @@ client.on('message', message => {
     if (message.content.startsWith(prefix + 'servers')) {
   		message.channel.send('⚠️ This command still has to be set up!');
   	}
+    if (message.content.startsWith(prefix + 'about')) {
+    message.channel.send({embed: {{ "plainText": "Hi! I am TeamCo7 Clan's bot", "title": "TeamCo7 Clan", "description": "I am a bot dedicated to assist Clan & Community members all allied to TeamCo7 Clan", "color": 53380 }});
+  
 });
 //  
 client.login(process.env.BOT_TOKEN);
