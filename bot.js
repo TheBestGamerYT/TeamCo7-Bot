84dd4fd4127752f539ba8acf11f38faf55b33ea9
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-01-02 00:28:35 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 var prefix = "!c7 "
 
 client.on('ready',() => {
-  console.log("I\'m Online !");
+  console.log("I\'m Online! Ready to launch!");
   client.user.setActivity('!c7 help | discord.me/teamco7', { type: 'PLAYING' })
     .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : '!c7 help | discord.gg/67gbBKH'}`))
     .catch(console.error);
@@ -14,7 +14,7 @@ client.on('message', message => {
 
   if (message.author === client.user) return;
   	if (message.content.startsWith(prefix + 'help')) {
-  		message.channel.send('Bot support: \n ```!c7 help > This message``` \n ```!c7 servers > Displays TeamCo7 Clan servers``` \n Note: This bot is still in BETA version! \n In case of DDoS hack, wait for response from TeamCo7 Clan!');
+  		message.channel.send('Bot support: \n !c7 help > This message \n !c7 servers > Displays TeamCo7 Clan servers \n Note: This bot is still in BETA version! \n In case of DDoS hack, wait for response from TeamCo7 Clan! \n Bot language: discord.js (javascript)');
   	}
     if (message.content.startsWith(prefix + 'servers')) {
   		message.channel.send('⚠️ This command still has to be set up!');
