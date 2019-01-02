4e8a1c74aa3cf6e6225b43c4d2f16814dfc21936
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-01-02 20:30:41 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', message => {
   		message.channel.send('⚠️ This command still has to be set up!');
   	}
     if (message.content.startsWith(prefix + 'about')) {
-    message.channel.send({embed: { "plainText": "Hi! I am TeamCo7 Clan's bot", "title": "TeamCo7 Clan Bot", "description": "Hi! I give assistence to TeamCo7 Clan members & allied clans or communities", "color": 53380 }});
+    message.channel.send({embed: { "plainText": "", "title": "About TeamCo7 Clan Bot", "description": "Hi! I\'m a bot that\'s serving: \n • TeamCo7 Clan members /n • Allied clans or communities", "color": 53380 }});
     }
 
 });
