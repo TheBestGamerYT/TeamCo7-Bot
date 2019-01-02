b48e351fa1915641f844f86564b7e15d701bd090
TheBestGamerYT <franci641@gmail.com>
2019-01-02 18:09:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-new Discord.RichEmbed(data); 
+new Discord.RichEmbed(data);
 
 var prefix = "!c7 "
 
@@ -12,7 +12,6 @@ client.on('ready',() => {
 })
 
 client.on('message', message => {
-
   if (message.author === client.user) return;
   	if (message.content.startsWith(prefix + 'help')) {
   		message.channel.send('Bot support: \n !c7 help   > This message \n !c7 servers > Displays TeamCo7 Clan servers \n Note: This bot is still in BETA version! \n In case of DDoS hack, wait for response! \n Bot language: discord.js (javascript)');
@@ -22,7 +21,8 @@ client.on('message', message => {
   	}
     if (message.content.startsWith(prefix + 'about')) {
     message.channel.send({embed: { "plainText": "Hi! I am TeamCo7 Clan's bot", "title": "TeamCo7 Clan", "description": "I am a bot dedicated to assist Clan & Community members all allied to TeamCo7 Clan", "color": 53380 }});
-  
+    }
+
 });
-//  
+//
 client.login(process.env.BOT_TOKEN);
