ff4b3f3ec313e5d90c5b9a402f3f5f1eb7581fbf
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-01-06 12:40:56 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,14 +10,19 @@ client.on('ready',() => {
     .catch(console.error);
 })
 
+// BOT SUPPORT
+
 client.on('message', message => {
   if (message.author === client.user) return;
   	if (message.content.startsWith(prefix + 'help')) {
-  		message.channel.send({embed: { "plainText": "", "title": "Bot Support", "description": "Commands: \n • !c7 help - Shows this message \n • !c7 servers - Displays clan & partnered servers \n • !c7 about - Shows bot information ", "color": 53380 }});
+  		message.channel.send({embed: { "plainText": "", "title": "Commands", "description": "Bot support: \n • !c7 help - Shows this message \n • !c7 servers - Displays clan & partnered servers \n • !c7 about - Shows bot information ", "color": 53380 }});
   	}
     if (message.content.startsWith(prefix + 'servers')) {
   		message.channel.send({embed: { "plainText": "", "title": "Servers", "description": "Clan: \n • TeamCo7 Clan \n Invite: https://discord.gg/TW9yC4V \n • FS| Softwares \n Invite: https://discord.gg/j39xBQa \n \n Communities: \n • GoodBoy00's Lounge \n Invite: https://discord.gg/8yyfzYY \n • PrinceRaj 592 \n Invite: https://discord.gg/rGu7F39", "color": 53380 }});
   	}
+  
+// CLAN & BOT INFORMATION
+  
     if (message.content.startsWith(prefix + 'about')) {
     message.channel.send({embed: { "plainText": "", "title": "About TeamCo7 Clan Bot", "description": "Hi! I\'m a bot that\'s serving: \n • TeamCo7 Clan members \n • Allied clans or communities", "color": 53380 }});
     }
