0500cf9158834747c9c4abc601752a91978280a3
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-01-11 22:39:40 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var prefix = "!c7 "
 client.on('ready', () => {
   console.log('I am ready!');
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`!c7 help | ${client.guilds.size} servers`);
 });
 
 client.on('message', message => {
@@ -15,7 +15,7 @@ client.on('message', message => {
   		message.channel.send({embed: { "plainText": "", "title": "Bot Support", "description": "Commands: \n • !c7 help - Shows this message \n • !c7 servers - Displays clan & partnered servers \n • !c7 about - Shows bot information ", "color": 53380 }});
   	}
     if (message.content.startsWith(prefix + 'servers')) {
-  		message.channel.send({embed: { "plainText": "", "title": "Servers", "description": "Clan: \n • TeamCo7 Clan \n Invite: https://discord.gg/TW9yC4V \n • FS| Softwares \n Invite: https://discord.gg/j39xBQa \n \n Partnered Communities: \n • GoodBoy00's Lounge \n Invite: https://discord.gg/8yyfzYY \n • PrinceRaj 592 \n Invite: https://discord.gg/rGu7F39 \n • TheBestGamerYT: \n Invite: https://discord.gg/urSjkJx", "color": 53380 }});
+  		message.channel.send({embed: { "plainText": "", "title": "Servers", "description": "Clan: \n • TeamCo7 Clan \n Invite: https://discord.gg/TW9yC4V \n • FS|C7 Bot Lounge \n Invite: https://discord.gg/j39xBQa \n \n Partnered Communities: \n • GoodBoy00's Lounge \n Invite: https://discord.gg/8yyfzYY \n • PrinceRaj 592 \n Invite: https://discord.gg/rGu7F39 \n • TheBestGamerYT: \n Invite: https://discord.gg/urSjkJx", "color": 53380 }});
   	}
     if (message.content.startsWith(prefix + 'about')) {
     message.channel.send({embed: { "plainText": "", "title": "About TeamCo7 Clan Bot", "description": "Hi! I\'m a bot that\'s serving: \n • TeamCo7 Clan members \n • Allied clans or communities", "color": 53380 }});
